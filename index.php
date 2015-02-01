6c20fbc286b288a23b0fef7cc7641b50ce7907ee
Brandon Monroy <frontend@webpuro.net>
2015-02-01 08:52:27 -0500

    WebPuro

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+include "app/config.php";
+include "app/detect.php";
+if ($page_name=='') {
+	include $browser_t.'/index.html';
+	}
+elseif ($page_name=='index.html') {
+	include $browser_t.'/index.html';
+	}
+elseif ($page_name=='about.html') {
+	include $browser_t.'/about.html';
+	}
+elseif ($page_name=='blog.html') {
+	include $browser_t.'/blog.html';
+	}
+elseif ($page_name=='portfolio.html') {
+	include $browser_t.'/portfolio.html';
+	}
+elseif ($page_name=='contact.html') {
+	include $browser_t.'/contact.html';
+	}
+elseif ($page_name=='contact-post.html') {
+	include $browser_t.'/contact.html';
+	include 'app/contact.php';
+	}
+else
+	{
+		include $browser_t.'/index.html';
+	}
+?>
